872dcedabf2b1e21f684f0f37971b7ab8eecd690
Iulia Pusca <pusca.iulia@gmail.com>
2018-09-26 19:49:27 +0300

    added new changes

--- testProjectTekwill/src/testPackageTekwill/TekwillTest.java
+++ testProjectTekwill/src/testPackageTekwill/TekwillTest.java
@@ -3,6 +3,10 @@ package testPackageTekwill;
 public class TekwillTest {
     public static void main(String[] args) {
 
-        System.out.println("Hello people!!!");
+        String name = "Pusca";
+        String surname = "Iulia";
+        String message;
+        message = name + " " +surname;
+        System.out.println("My name is: " + message);
     }
 }
